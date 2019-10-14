49c78968b446113b8cf1af996d6534c166a33d4a
KennedyOn <kennedy2000silva@hotmail.com>
2019-10-14 16:51:54 -0300

    Transformei em routes a forma de chamar as pag

--- src/App.js
+++ src/App.js
@@ -2,34 +2,20 @@ import React from 'react';
 import './App.css';
 
 import SideBar from './components/navbar/navbar';
-import Highlights from './components/highLights/highlights';
+import Routes from './routes';
+import Highlights from './components/highLights/highlights'
+
 /*import menu from './assets/menu.svg';
 import joystickIcon from './assets/icons8_controller_64px_1@2x.png';
 */
 
 
 function App() { 
-  
-
- 
   return (
-    <>
-      
-        <div className="container">         
 
-          <div id="App">
-            <SideBar />      
-          </div>
-          <div className="content">
-
-          </div>
-
-          <div className="content">
-            <Highlights /> 
-          </div>          
-
-        
-      </div>
+    <>      
+      <SideBar />
+      <Routes />  
     </>
   );
 }
