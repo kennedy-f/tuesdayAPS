b709a8a95fdaa24923b8e3bbaeca036e281f34e9
KennedyOn <kennedy2000silva@hotmail.com>
2019-10-17 15:43:31 -0300

    Criando pagina de login

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,54 @@
+import React from 'react'; 
+import './style.css';
+
+import Facebook_logo from '../../assets/facebook_logo.svg'
+import Google_logo from '../../assets/search.svg'
+
+export default props => {
+    return (
+        <>
+           <div className="container_login">
+               <div className="content_login">
+                   <h1> </h1>
+               </div>
+               <div className="content_box">
+                   <div className="login">
+                       <span> Usuario / email </span>
+                       <input type="text" name="user" id="user" placeholder="usuario / e-mail"/>
+                       <span> senha </span>
+                       <input type="text" name="password" id="password" placeholder="senha"/>
+                       <button className="confirmation_btn">
+                           <span className="button_text">
+                               Entrar
+                           </span>
+                       </button>                       
+                   </div>
+                   <div className="login">
+                       <h3> Ainda não é cadastrado </h3>
+                        <span>  crie sua conta  </span>
+                        <input type="text" placeholder="Nome de Usuario"/>
+                        <input type="email" placeholder="Email" />
+                        <input type="email" placeholder="Confirme o Email" />
+                        <input type="password" placeholder="Senha" />
+                        <input type="password" placeholder="Confirme a senha" />
+                        <button className="confirmation_btn">
+                            <span className="button_text"> Crie sua conta</span>
+                        </button>
+                   </div>
+
+               </div>
+                   <h2> ou </h2>
+
+                <button id="facebook_btn">                     
+                    <img src={Facebook_logo} alt="facebook_login" id="facebook_icon" />
+                    <span> faça login pelo <strong> facebook </strong></span>
+                </button>
+                <button id="facebook_btn">
+                    <img src={Google_logo} alt="facebook_login" id="facebook_icon" />
+                    <span> faça login pelo <strong> Google </strong></span>
+                </button>
+           </div>
+        </>
+
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './Pages/Home'; 
 import Game from './Pages/Games'; 
+import Login from './Pages/Login'; 
 
 export default function Routes (){
     return (
@@ -10,6 +11,7 @@ export default function Routes (){
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/Games123" component={Game} />
+                <Route path="/login" component={Login}/>
             </Switch>
         </BrowserRouter>
     )
