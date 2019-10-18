63b4852a9682a60173a48a466370d02665b0162f
KennedyOn <kennedy2000silva@hotmail.com>
2019-10-18 16:00:26 -0300

    Adicionada verificação de login

--- Backend/src/controller/UserController.js
+++ Backend/src/controller/UserController.js
@@ -3,30 +3,46 @@ const User = require('../model/User');
 
 module.exports = {
     async store(req, res) {
-        const { email, pass, username } = req.body;
+        const { email, password, username } = req.body;
 
-        let user = await User.findOne({ username });
-        let ifEmail = await User.findOne({ email });
+        let user = await User.findOne({ username }); // declaração de uma variavel user, que vai buscar o username
+        let ifEmail = await User.findOne({ email }); // vai buscar o email 
         
 
-        if (!user && !ifEmail) {
-
+        if (!user && !ifEmail) { //reparem no '!', que quer dizer se user ou email não existirem então faça 
             user = await User.create({ 
                 email,
                 username, 
-                pass
-            });
-            
-
+                password
+            });          
         }
 
-        if  (user) {
-            return res.json({ Usernameexiste : "true"});
+        if  (user) { // aqui é se o user existir
+            return res.json({ UsernameExist: "true"});
         } ;
-        if (email) {
-            return res.json({ Emailexiste : "true"});
+
+        if (email) { // e aqui se o email existir, criei em partes para deixar um aviso diferente para cada 
+            return res.json({ EmailExist : "true"});            
         };
 
         return res.json(user);
+    },
+
+    async show (req, res){
+        const { password, username } = req.body; 
+
+        
+        let user = await User.findOne({ username }).select('+password'); 
+        let ifUsername = await User.findOne({username}).select('+username');
+        if (!ifUsername) {
+            return res.status(400).send('Login não existe');
+        }
+        if (user.password == password){
+            return res.json(user); 
+        }
+        
+        
+        return res.status(400).send('Senha errada');
+
     }
 };
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -9,9 +9,9 @@ const UserController = require('./controller/UserController');
 const routes = express.Router(); 
 const uploads = multer(uploadConfig); 
 
-routes.post('/createauser', UserController.store);
+routes.post('/login', UserController.store);
+routes.get('/login', UserController.show);
 routes.post('/addGame', uploads.single('thumbnail'), GameController.store);
-
 
 
 module.exports = routes;
