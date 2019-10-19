0642b4ce48e239d18d9903a29af4598bbf87d751
KennedyOn <kennedy2000silva@hotmail.com>
2019-10-18 21:47:23 -0300

    adicionada troca de slide

--- Frontend/src/Pages/Home/index.js
+++ Frontend/src/Pages/Home/index.js
@@ -1,60 +1,65 @@
 import React from 'react';
 import './style.css';
-import anime from 'animejs';
+import Anime from 'animejs';
 
-import game1 from '../../../src/assets/CapaForzaHorizon4Wivernz@2x.png'
-import game2 from '../../../src/assets/FundoAssassinsCreedUnityGoldEditionELAMIGOSWivernz.png'
+import Game1 from '../../../src/assets/CapaForzaHorizon4Wivernz@2x.png'
+import Game2 from '../../../src/assets/FundoAssassinsCreedUnityGoldEditionELAMIGOSWivernz@2x.png'
 
 // <li> <img src={game1} alt="" /> </li> 
 //<li> <img src={game1} alt="" /> </li>
 function textAnimationIn() {
-    anime({
+    Anime({
         targets: '.middleText',
         opacity: '1',
         translateX: 50,
         duration: 3000,
         stagger: 300
     });
-
 }
+
 function textAnimationOut() {
-    anime({
+    Anime({
         targets: '.middleText',
         opacity: '0'
     });
-    anime({
+    Anime({
         targets: 'text',
         translateX: 0,
     });
 }
 
-
-
-
+var game = {
+    thumbnail : Game1,
+    title : 'Forza Horizon 4'
+} 
+console.log(game.thumbnail);
 export default props => {
     return (
         <>
             <div className="topContainer">
                 <div className="highlights-games">
                     <div className="content" onMouseOver={textAnimationIn} onMouseOut={textAnimationOut}>
-                        <img src={game1} alt="" className="slide" />
+                        <img src={game.thumbnail} alt="" className="slide" />
                         <div className="middleText">
-                            <div className="text"> Forza Horizon 4 </div>
+                            <div className="text"> {game.title} </div>
                         </div>
                     </div>
+
                     <div className="content">
                         <div className="preview">
                             <ul>
-                                <li id="game1" > <img src={game1} alt="" /> </li>
-                                <li id="game2" > <img src={game2} alt="" /> </li>
+                                <li id="game1" > <img src={Game1} alt="" /> </li>
+                                <li id="game2" > <img src={Game2} alt="" onMouseOver={() => {
+                                    game.thumbnail = Game2; 
+                                    console.log('troca')
+                                    console.log(game.thumbnail);}
+                                }/> </li>
                             </ul>
                             <ul>
-                                <li id="game3" > <img src={game1} alt="" /> </li>
-                                <li id="game4" > <img src={game2} alt="" /> </li>
+                                <li id="game3" > <img src={Game1} alt="" /> </li>
+                                <li id="game4" > <img src={Game2} alt="" /> </li>
                             </ul>
-
-
-                        </div>
+                        </div>                        
 
                         <div className="highlightDesc">
                             <h1>Forza Horizon 4</h1>
@@ -74,14 +79,15 @@ export default props => {
                                 porra essa, essa quebrou uhu, veja o tamanho da imagem tá esticando <br />
                                 interessante
                             </span>
+
                             <h3 id="valor" > R$ 150,00</h3>
+
                             <div className="buttonArea">
                                 <div></div>
                                 <button> adicionar ao carrinho</button>
                                 <button> adicionar ao carrinho</button>
                             </div>
                         </div>
-
 
                     </div>
                 </div>
