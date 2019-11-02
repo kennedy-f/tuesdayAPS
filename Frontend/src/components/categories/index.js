c50f79b9d15c04bed9a52011655bd4c7ea390994
KennedyOn <kennedy2000silva@hotmail.com>
2019-11-02 14:53:28 -0300

    Arrumando os slides

--- Frontend/src/components/categories/index.js
+++ Frontend/src/components/categories/index.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect } from 'react';
 
+import {Carousel} from 'react-bootstrap';
+
+
 import './style.css';
 
 import api from '../../services/api';
@@ -20,22 +23,19 @@ export default function Categories({ catg }){
     }, [catg]);  
 
     return (
-        <div className="categories">
-            <h3 className="categoryTitle"> {catg} </h3>
-            <ul>
-                {games.map(game => (
+        <>
+            <div className="categories">
+                <Carousel >
+                    {games.map(game => (
+                        <Carousel.Item key={game._id}>
+                            <img src={game.thumbnail_url} alt=""/>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+              
+            </div>
+        </>
 
-                    <li key={game._id} >
-                        <div className="gameContainer">
-                            <img src={game.thumbnail_url} alt="" />
-
-                        </div>
-
-                    </li>
-                ))}
-            </ul>
-
-        </div>
    
     ); 
 }
@@ -57,5 +57,27 @@ export default function Categories({ catg }){
     </ul>
 
 </div>
+
+
+     <div className="categories">
+            <h3 className="categoryTitle"> {catg} </h3>
+            <ul>
+                {games.map(game => (
+
+                    <li key={game._id} >
+                        <div className="gameContainer">
+                            <img src={game.thumbnail_url} alt="" />
+
+                        </div>
+
+                    </li>
+                ))}
+            </ul>
+
+
+
+        </div>
+
+
 
 */
