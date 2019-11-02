0fc21870194b04f6caaabb1ceb95e42cd891278d
KennedyOn <kennedy2000silva@hotmail.com>
2019-11-02 14:53:55 -0300

    adicionado o src do bootstrap

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 /*import SideBar from './components/navbar/navbar';*/
 
 import Routes from './routes';
