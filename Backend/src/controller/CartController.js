3867b827bceab1774599ffbe2b03a526126ef0e4
KennedyOn <kennedy2000silva@hotmail.com>
2019-11-11 23:03:39 -0300

    arrumado o problema

--- Backend/src/controller/CartController.js
+++ Backend/src/controller/CartController.js
@@ -3,21 +3,21 @@ const Bag = require('../model/Bag');
 module.exports = {      
     
     async store(req, res){        
-        const { _id, bag_id, id_game} = req.headers;  
+        const { _id, bag_id } = req.headers;  
         const { id_game } = req.query; 
 
-        const gameExist = await Bag.findOne({ id_user: _id }).findOne({ id_game: game_id});
+        const gameExist = await Bag.findOne({ id_user: _id }).findOne({ id_game});
         const bag = await Bag.find({ id_user: _id}); 
         
         if (!gameExist){
-            const upBag = await Bag.updateOne({ _id: bag_id }, { $push: { upsert: true, id_game: game_id } })
+            const upBag = await Bag.updateOne({ _id: bag_id }, { $push: { upsert: true, id_game } })
             const finalBag = await Bag.findOne({ id_user: _id }).populate('id_game');            
 
-            return res.json(game_id); 
+            return res.json(id_game); 
         }
         const finalBag = await Bag.findOne({ id_user: _id }).populate('id_game');    
 
-        return res.json(game_id); 
+        return res.json(id_game); 
         
     }
 
